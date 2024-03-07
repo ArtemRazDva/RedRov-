79dacb22b343942f39a402d4bd77d2e8cdfd67a1
ArtemRazdymakha <razdymakha91@mail.ru>
2024-03-06 19:11:01 -0500

    new push

--- src/first_project.java
+++ src/first_project.java
@@ -7,30 +7,6 @@ public class first_project {
 
     public static void main(String[] args) {
 
-//        String stranger = "Who are you?";
-//        int a;
-//        a = 1;
-//        int b = 4;
-//        int c = b - a;
-//        long f = 2147483646542123214L;
-//        int h = 54365464;
-//        String soul = "Шарик должен сдохнуть";
-//        String box = "Шарик держись";
-//        if (a > c) {
-//            System.out.println(soul);
-//        } else if (a < c) {
-//            System.out.println(box);
-//        }
-//        for (int z = 4; z < 0; z++) {
-//            System.out.println(soul);
-//
-//        }
-//
-//        int[] array = new int[100];//{10, 20, 54, 86};
-//        array[98] = 3333;
-//        for (int i = 0; i < array.length; i++) {
-//            System.out.println(array[i]);
-//        }
 
         int[] bob = {1, -4, 4, -28, 3};
         int max  = 0;
